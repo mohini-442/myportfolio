0f7985d26ec8f608af249f0b86ce19a2c5164c4c
Anshul sharma <anshhulkaushik@gmail.com>
2024-05-02 16:25:05 +0530

    ok

--- Portfolio-main/src/Component/Skills.jsx
+++ Portfolio-main/src/Component/Skills.jsx
@@ -1,63 +1,52 @@
-import React from "react";
-import "./Global.css"
+import React, { useEffect, useState } from 'react';
 
-const Skills = () => {
+function ProgressBar({ title, progress }) {
+  const [count, setCount] = useState(0);
+  const MAX = parseInt(progress);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (count < MAX) {
+        setCount(prevCount => prevCount + 1);
+      } else {
+        clearInterval(interval);
+      }
+    }, 20);
+
+    return () => clearInterval(interval);
+  }, [count, MAX]);
+
   return (
-    <>
-      <div className="Skills p-3 flex justify-center items-center my-4">
-        <div className="skills-Text-part w-3/5 p-3 rounded-lg flex flex-col items-center boxShadow" style={{backgroundImage: "linear-gradient(to top, #3f51b1 0%, #5a55ae 13%, #7b5fac 25%, #8f6aae 38%, #a86aa4 50%, #cc6b8e 62%, #f18271 75%, #f3a469 87%, #f7c978 100%)"}}>
-          <h1 className="text-4xl text-white font-bold text-center">My Skills</h1>
-          <p className="text-white text-xl text-center my-5 font-bold">
-          
-           "Proficient in HTML, CSS, and JavaScript, I design and develop captivating web interfaces with seamless functionality. Leveraging Tailwind CSS, I bring modern, 
-              responsive designs to life, ensuring a polished and user-friendly experience."
-          </p>
-
-          <div className="skills-Ui flex gap-5 my-5 flex-wrap justify-center items-center">
-            <div className="border-2 boxShadow border-white w-64 h-28 rounded-lg flex flex-col items-center justify-center hover:scale-110 hover:bg-black transition-all delay-100">
-              <img
-                src="https://clipground.com/images/html-logo-png-3.png"
-                className="w-24"
-              />
-              <h1 className="text-white font-bold text-xl">HTML</h1>
-            </div>
-
-            <div className="border-2 boxShadow border-white w-64 h-28 rounded-lg flex flex-col items-center justify-center hover:scale-110 hover:bg-black transition-all delay-100">
-              <img
-                src="https://logospng.org/download/css-3/logo-css-3-2048.png"
-                className="w-16"
-              />
-              <h1 className="text-white font-bold text-xl">CSS</h1>
-            </div>
-
-            <div className="border-2 boxShadow border-white w-64 h-28 rounded-lg flex flex-col items-center justify-center hover:scale-110 hover:bg-black transition-all delay-100">
-              <img
-                src="https://logosdownload.com/logo/javascript-logo-big.png"
-                className="w-12"
-              />
-              <h1 className="text-white font-bold text-xl">Javascript</h1>
-            </div>
-
-            <div className="border-2 boxShadow border-white w-64 h-28 rounded-lg flex flex-col items-center justify-center hover:scale-110 hover:bg-black transition-all delay-100">
-              <img
-                src="https://logodix.com/logo/1658364.png"
-                className="w-24"
-              />
-              <h1 className="text-white font-bold text-xl">Reactjs</h1>
-            </div>
-
-            <div className="border-2 boxShadow border-white w-64 h-28 rounded-lg flex flex-col items-center justify-center hover:scale-110 hover:bg-black transition-all delay-100">
-              <img
-                src="https://static-00.iconduck.com/assets.00/tailwind-css-icon-2048x1229-u8dzt4uh.png"
-                className="w-24"
-              />
-              <h1 className="text-white font-bold text-xl">Tailwind</h1>
-            </div>
-          </div>
+    <div className=" w-[150px] sm:w-[200px] h-auto">
+      <div className="skill w-[150px] h-[150px] sm:w-[200px] sm:h-[200px] relative">
+        <div className="progress relative z-10 w-10 h-10  sm:w-48 sm:h-48  ">
+         </div>
+         <div
+            className="absolute top-0 left-0 w-full h-full  rounded-full "
+            style={{
+              clipPath: ` `,
+              background: `conic-gradient(#B75CFF ${Math.min(count, MAX)}%, #212428 0)`
+            }}
+          >
+          <span className="progress-number absolute left-1/2 top-1/2 z-20 -translate-x-1/2 -translate-y-1/2 ">{count}%</span>
         </div>
       </div>
-    </>
+      <span className="title text-[#B75CFF] ff-poppins text-center block mt-2 sm:mt-5 text-lg sm:text-3xl">{title}</span>
+    </div>
   );
-};
+}
 
-export default Skills;
+function SkillsSection() {
+  return (
+    <section className="flex justify-center flex-wrap  items-center h-[90vh]  bg-black">
+      <div className='flex justify-center flex-wrap  items-center gap-4 sm:gap-6'>
+        <ProgressBar title="HTML" progress={80} />
+        <ProgressBar title="CSS" progress={70} />
+        <ProgressBar title="JavaScript" progress={65} />
+        <ProgressBar title="React" progress={50} />
+      </div>
+    </section>
+  );
+}
+
+export default SkillsSection;
