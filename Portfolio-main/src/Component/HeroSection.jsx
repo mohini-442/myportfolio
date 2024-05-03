399eb76a1f829979fd00e8067051dd09e10dba27
mohini-442 <mohinikarwasra598@gmail.com>
2024-05-02 15:01:13 -1200

    commit2

---
+++ Portfolio-main/src/Component/HeroSection.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { FaInstagram } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+
+const HeroSection = () => {
+  return (
+    <>
+      <section
+        className="sm:h-[90vh] w-full px-36 mx-auto sm:flex-wrap flex flex-col justify-center bg-black"
+      >
+        <div className="flex ">
+          <div className=" lg:w-1/2 ">
+            <div className="Social-links flex gap-2">
+              <div
+                className="Instagram border-2 border-[#B75CFF] w-12 h-12 flex items-center justify-center rounded-full"
+                style={{ backgroundImage: "linear-gradient(to top, #3f51b1 0%, #5a55ae 13%, #7b5fac 25%, #8f6aae 38%, #a86aa4 50%, #cc6b8e 62%, #f18271 75%, #f3a469 87%, #f7c978 100%)" }}
+              >
+                <a href="https://www.instagram.com/confident_coder/">
+                  <FaInstagram className="text-2xl  text-white hover:scale-110" />
+                </a>
+              </div>
+              <div
+                className="Github border-2 border-[#B75CFF] w-12 h-12 flex items-center justify-center rounded-full"
+                style={{ backgroundImage: "linear-gradient(to top, #3f51b1 0%, #5a55ae 13%, #7b5fac 25%, #8f6aae 38%, #a86aa4 50%, #cc6b8e 62%, #f18271 75%, #f3a469 87%, #f7c978 100%)" }}
+              >
+                <a href="https://github.com/vednidhi930">
+                  <FaGithub className="text-2xl text-white hover:scale-110" />
+                </a>
+              </div>
+            </div>
+
+            <div className="My-detail  pt-4 my-2">
+              <h4 className="text-white text-2xl ff-poppins ">Hello!</h4>
+              <h1 className="text-white text-6xl  font-bold ff-poppins pt-2 text-nowrap">
+                I'm <span className="text-6xl ff-poppins text-[#B75CFF]">Mohini</span>
+              </h1>
+              <h3 className="text-white text-3xl ff-poppins pt-1">Frontend developer</h3>
+              <p className="text-white ff-poppins my-2 text-2xl">
+                I'M vednidhi,a passionate web developer with a knack for turning
+                ideas into captivating online experiences.
+              </p>
+            </div>
+          </div>
+
+          <div className="Right-part flex lg:w-2/4 me-7">
+            <img src="https://www.dharmishi.com/img/vectors/developer.png" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default HeroSection;
---
+++ Portfolio-main/src/Component/Navbar.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import { CiMenuFries } from "react-icons/ci";
+import { MdCancel } from "react-icons/md";
+import "./Global.css"
+import { NavLink } from "react-router-dom";
+
+
+const Navbar = () => {
+  const [menu, setMenu] = useState(false);
+  return (
+    <>
+      <div className="bg-black border-b-2 border-[#B75CFF] min-h-[10vh] flex items-center">
+        <nav className="w-full px-24 lg:px-36 mx-auto relative ">
+          <div className="flex justify-between items-center">
+            <div className="">
+              {/* <h3 className=""><a href=""><NavLink className="" to="/">MyPortFolio</NavLink></a></h3> */}
+              <a href=""><h1 className="text-[#B75CFF] ff-poppins text-5xl font-bold">MyPortfolio</h1></a>
+            </div>
+            <div className="Right-Navbar">
+              <ul className="flex gap-5 justify-between 
+            items-center 
+            text-2xl ff-poppins
+          text-white
+            text-nowrap
+            duration-[0.5s]
+            cursor-pointer"
+              >
+                <li>
+                  <NavLink to="/" className="hover:text-[#B75CFF]">Home</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/Skills" className="hover:text-[#B75CFF]">Skills</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/About" className="hover:text-[#B75CFF]">About Me</NavLink>
+                </li>
+
+                <li>
+                  <NavLink to="/Project" className="hover:text-[#B75CFF]">Projects</NavLink>
+                </li>
+
+                <li>
+                  <NavLink to="/Contact" className="hover:text-[#B75CFF]">Contact</NavLink>
+                </li>
+                <CiMenuFries className="text-4xl Menu-icon" onClick={() => setMenu(!menu)} />
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+      <div className={`h-screen w-52 p-3 border-4 border-black 
+           ${menu === false ? "hidden" : "block"}  
+            absolute 
+            top-0
+            right-0`} style={{ background: "rgba(10, 61, 98,1.0)" }}>
+        <ul className="flex justify-between
+            flex-col
+            items-center pt-2 
+            text-xl font-serif
+          text-white
+            text-nowrap
+            cursor-pointer
+            gap-3
+            "
+        >
+          <li>
+            <NavLink to="/" className="hover:text-[#B75CFF]" onClick={() => setMenu(false)}>Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/Skills" className="hover:text-[#B75CFF]" onClick={() => setMenu(false)}>Skills</NavLink>
+          </li>
+          <li>
+            <NavLink to="/About" className="hover:text-[#B75CFF]" onClick={() => setMenu(false)}>About Me</NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/Project" className="hover:text-[#B75CFF]" onClick={() => setMenu(false)}>Projects</NavLink>
+          </li>
+
+          <li>
+            <NavLink to="/Contact" className="hover:text-[#B75CFF]">Contact</NavLink>
+          </li>
+
+          <MdCancel className="text-4xl absolute top-0 right-0 hover:text-[#B75CFF]" onClick={() => setMenu(false)} />
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
